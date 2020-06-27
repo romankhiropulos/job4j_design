854686f99640e7466860bb7b045ca2727c960d39
romankhiropulos <romankhiropulos@gmail.com>
2020-06-27 21:18:21 +0300

    5.1.1. Итератор для двухмерного массива int[][] [#310080]

---
+++ chapter_001_Data_structures_and_algorithms/src/main/java/ru/job4j/it/MatrixIt.java
@@ -0,0 +1,66 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MatrixIt implements Iterator<Integer> {
+    private final int[][] data;
+    private int row = 0;
+    private int column = 0;
+
+    public MatrixIt(int[][] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (data.length == 1 && data[row].length == 0) {
+            return false;
+        }
+
+        if (row < data.length && column < data[row].length) {
+            return true;
+        } else if (row == data.length) {
+            return false;
+        } else {
+            column = 0;
+            row++;
+            while (row < data.length && data[row].length == 0) {
+                row++;
+            }
+            return row != data.length;
+        }
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[row][column++];
+    }
+
+    public static void main(String[] args) {
+        int[][] array = new int[3][4];
+        array[0] = new int[]{5, 7, 3, 17};
+        array[1] = new int[]{7, 1, 12};
+        array[2] = new int[]{8, 1, 2, 3};
+
+        for (int i = 0; i < array.length; i++) {  // идём по строкам
+            for (int j = 0; j < array[i].length; j++) { // идём по столбцам
+                System.out.print(" " + array[i][j] + " "); //вывод элемента
+            }
+            System.out.println(); // перенос строки ради визуального сохранения табличной формы
+        }
+
+        int[][] in = {
+                {}
+        };
+        for (int i = 0; i < in.length; i++) {  // идём по строкам
+            for (int j = 0; j < in[i].length; j++) { // идём по столбцам
+                System.out.print(" " + in[i][j] + " "); //вывод элемента
+            }
+            System.out.println(); // перенос строки ради визуального сохранения табличной формы
+        }
+    }
+}
