10ba54854067fcffbb0a8601fe15b5a903b7c391
romankhiropulos <romankhiropulos@gmail.com>
2020-06-30 21:40:54 +0300

    5.1.4. FlatMap для Iterator<Iterator> [#310078]

--- chapter_001_Data_structures_and_algorithms/src/main/java/ru/job4j/Main.java
+++ chapter_001_Data_structures_and_algorithms/src/main/java/ru/job4j/Main.java
@@ -1,7 +1,30 @@
 package ru.job4j;
 
+import java.util.*;
+
 public class Main {
+
+    public static void processIterator(String[] array) {
+        // write your code here
+        List<String> strings = new ArrayList<>(List.of(array));
+        ListIterator<String> listIterator = strings.listIterator();
+        String row;
+        while (listIterator.hasNext()) {
+            row = listIterator.next();
+            if (row.charAt(0) == 'J') {
+                listIterator.set(row.substring(1));
+            } else {
+                listIterator.remove();
+            }
+        }
+        while (listIterator.hasPrevious()) {
+            System.out.println(listIterator.previous());
+        }
+    }
+
+    /* Do not change code below */
     public static void main(String[] args) {
-        System.out.println("Hello, Java!");
+        Scanner scanner = new Scanner(System.in);
+        processIterator(scanner.nextLine().split(" "));
     }
 }
