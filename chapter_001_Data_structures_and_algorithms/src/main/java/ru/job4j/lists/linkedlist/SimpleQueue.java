a74eee393e1b75a6c0b1e969b58acd06cc0c6859
romankhiropulos <romankhiropulos@gmail.com>
2020-07-17 23:09:25 +0300

    Fixed SimpleQueue, added two counters for In and Out stacks

--- chapter_001_Data_structures_and_algorithms/src/main/java/ru/job4j/lists/linkedlist/SimpleQueue.java
+++ chapter_001_Data_structures_and_algorithms/src/main/java/ru/job4j/lists/linkedlist/SimpleQueue.java
@@ -3,33 +3,32 @@ package ru.job4j.lists.linkedlist;
 import java.util.NoSuchElementException;
 
 public class SimpleQueue<T> {
+    private int inCount;
+    private int outCount;
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
 
     private void exchange(SimpleStack<T> in, SimpleStack<T> out) {
-        T value = in.pop();
-        if (value == null) {
-            return;
-        }
-        while (value != null) {
-            out.push(value);
-            value = in.pop();
+        while (inCount != 0) {
+            inCount--;
+            outCount++;
+            out.push(in.pop());
         }
     }
 
     public T poll() {
-        T result = out.pop();
-        if (result == null) {
-            exchange(in, out);
-            result = out.pop();
-        }
-        if (result == null) {
+        if (outCount == 0 && inCount == 0) {
             throw new NoSuchElementException();
         }
-        return result;
+        if (outCount == 0) {
+            exchange(in, out);
+        }
+        outCount--;
+        return out.pop();
     }
 
     public void push(T value) {
         in.push(value);
+        inCount++;
     }
 }
