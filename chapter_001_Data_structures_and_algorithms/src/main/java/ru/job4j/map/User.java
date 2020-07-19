723f3f5ed1e04d746c3fd01cb3eef8c4dd79e488
romankhiropulos <romankhiropulos@gmail.com>
2020-07-19 21:34:13 +0300

    Added equals() and hashCode()

--- chapter_001_Data_structures_and_algorithms/src/main/java/ru/job4j/map/User.java
+++ chapter_001_Data_structures_and_algorithms/src/main/java/ru/job4j/map/User.java
@@ -16,6 +16,32 @@ public class User {
         this.birthday = birthday;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        if (children != user.children) {
+            return false;
+        }
+        if (!name.equals(user.name)) {
+            return false;
+        }
+        return birthday.equals(user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + children;
+        result = 31 * result + birthday.hashCode();
+        return result;
+    }
+
     public static void main(String[] args) {
         User user1 = new User("Roman", 0, new GregorianCalendar(1993, 12, 21));
         User user2 = new User("Roman", 0, new GregorianCalendar(1993, 12, 21));
@@ -25,6 +51,7 @@ public class User {
         map.put(user2, 6);
 
         for (Map.Entry<User, Object> entry : map.entrySet()) {
+            System.out.println("HashCode: " + entry.getKey().hashCode() + " Value: " + entry.getValue());
             System.out.println("Key: " + entry.getKey() + " Value: " + entry.getValue());
         }
     }
