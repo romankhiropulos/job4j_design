f6a2a2c7cc9860689c2e738932550737da03984c
romankhiropulos <romankhiropulos@gmail.com>
2020-10-04 15:32:38 +0300

    ConnectionDemo with app.properties

---
+++ chapter_003_Databases/src/main/java/ru/job4j/conndemo/ConnectionDemo.java
@@ -0,0 +1,30 @@
+package ru.job4j.conndemo;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionDemo {
+    private static final String PROPS = "/app.properties";
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        try (InputStream is = ConnectionDemo.class.getResourceAsStream(PROPS)) {
+            Properties props = new Properties();
+            props.load(is);
+            try (Connection connection = DriverManager.getConnection(props.getProperty("db.url"),
+                    props.getProperty("db.user"), props.getProperty("db.password"))) {
+                DatabaseMetaData metaData = connection.getMetaData();
+                System.out.println(metaData.getUserName());
+                System.out.println(metaData.getURL());
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Invalid config file " + PROPS);
+        }
+
+    }
+}
