52adb74ee3cd4b68a1896cb3bdb5680e64402dac
romankhiropulos <romankhiropulos@gmail.com>
2020-11-08 22:57:09 +0300

    Add Report Engine

--- chapter_005_TDD/src/main/java/ru/job4j/srp/ReportEngine.java
+++ chapter_005_TDD/src/main/java/ru/job4j/srp/ReportEngine.java
@@ -1,5 +1,7 @@
 package ru.job4j.srp;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class ReportEngine {
@@ -9,15 +11,85 @@ public class ReportEngine {
         this.store = store;
     }
 
+//    Через месяц применения системы отчетности отдел программистов потребовал ответы в виде html.
+//
+//    Отдел бухгалтерии попросил изменить вид зарплаты.
+//
+//    Отдел HR попросил выводить сотрудников в порядке убывания зарплаты и убрать поля даты найма и увольнения.
+
     public String generate(Predicate<Employee> filter) {
         StringBuilder text = new StringBuilder();
-        text.append("Name; Hired; Fired; Salary");
+        text.append("Name; Hired; Fired; Salary;")
+                .append(System.lineSeparator());
         for (Employee employee : store.findBy(filter)) {
             text.append(employee.getName()).append(";")
                     .append(employee.getHired()).append(";")
                     .append(employee.getFired()).append(";")
-                    .append(employee.getSalary()).append(";");
+                    .append(employee.getSalary()).append(";")
+                    .append(System.lineSeparator());
         }
         return text.toString();
+    }
+
+    public String generateForProgrammers(Predicate<Employee> filter) {
+        StringBuilder htmlText = new StringBuilder();
+        htmlText.append("<!DOCTYPE html>").append(System.lineSeparator());
+        htmlText.append("<html>").append(System.lineSeparator());
+        htmlText.append("<head>").append(System.lineSeparator());
+        htmlText.append("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">")
+                .append(System.lineSeparator());
+        htmlText.append("<title>Report for programmers</title>").append(System.lineSeparator());
+        htmlText.append("</head>").append(System.lineSeparator());
+        htmlText.append("<body>").append(System.lineSeparator());
+        htmlText.append("<h1>Report for programmers</h1>").append(System.lineSeparator());
+        htmlText.append("<p>Name; Hired; Fired; Salary;</p>").append(System.lineSeparator());
+        htmlText.append("<p>Name; Hired; Fired; Salary;</p>").append(System.lineSeparator());
+        for (Employee employee : store.findBy(filter)) {
+            htmlText.append("<p>").append(employee.getName()).append(";").append("<p>")
+                    .append(System.lineSeparator())
+                    .append("<p>").append(employee.getHired()).append(";").append("<p>")
+                    .append(System.lineSeparator())
+                    .append("<p>").append(employee.getFired()).append(";").append("<p>")
+                    .append(System.lineSeparator())
+                    .append("<p>").append(employee.getSalary()).append(";").append("<p>")
+                    .append(System.lineSeparator());
+        }
+        htmlText.append("</body>").append(System.lineSeparator());
+        htmlText.append("/html").append(System.lineSeparator());
+
+        return htmlText.toString();
+    }
+
+    public String generateForBookkeeping(Predicate<Employee> filter) {
+        double tax = 0.13;
+        StringBuilder bookkeepingText = new StringBuilder();
+        bookkeepingText.append("Name; Hired; Fired; Salary;")
+                .append(System.lineSeparator());
+        for (Employee employee : store.findBy(filter)) {
+            bookkeepingText.append(employee.getName()).append(";")
+                    .append(employee.getHired()).append(";")
+                    .append(employee.getFired()).append(";")
+                    .append(employee.getSalary() * (1 - tax)).append(";")
+                    .append(System.lineSeparator());
+        }
+
+        return bookkeepingText.toString();
+    }
+
+    public String generateForHR(Predicate<Employee> filter) {
+        StringBuilder hrText = new StringBuilder();
+        hrText.append("Name; Hired; Fired; Salary;")
+                .append(System.lineSeparator());
+        List<Employee> list = store.findBy(filter);
+        list.sort(Comparator.comparingDouble(Employee::getSalary));
+        int size = list.size();
+        for (int i = size - 1; i >= 0; i--) {
+            Employee employee = list.get(i);
+            hrText.append(employee.getName()).append("; ")
+                    .append(employee.getSalary()).append(";")
+                    .append(System.lineSeparator());
+        }
+
+        return hrText.toString();
     }
 }
--- chapter_005_TDD/src/test/java/ru/job4j/srp/ReportEngineTest.java
+++ chapter_005_TDD/src/test/java/ru/job4j/srp/ReportEngineTest.java
@@ -1,9 +1,11 @@
 package ru.job4j.srp;
 
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.Matchers.is;
 import org.junit.Test;
+
 import java.util.Calendar;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class ReportEngineTest {
 
@@ -22,6 +24,83 @@ public class ReportEngineTest {
                 .append(worker.getFired()).append(";")
                 .append(worker.getSalary()).append(";")
                 .append(System.lineSeparator());
-//        assertThat(engine.generate(em -> true), is(expect.toString()));
+        assertThat(engine.generate(em -> em.getName().equals("Ivan")), is(expect.toString()));
+    }
+
+    @Test
+    public void whenGenerateForProgrammers() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        store.add(worker);
+        ReportEngine engine = new ReportEngine(store);
+        StringBuilder expect = new StringBuilder();
+        expect.append("<!DOCTYPE html>").append(System.lineSeparator());
+        expect.append("<html>").append(System.lineSeparator());
+        expect.append("<head>").append(System.lineSeparator());
+        expect.append("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">")
+                .append(System.lineSeparator());
+        expect.append("<title>Report for programmers</title>").append(System.lineSeparator());
+        expect.append("</head>").append(System.lineSeparator());
+        expect.append("<body>").append(System.lineSeparator());
+        expect.append("<h1>Report for programmers</h1>").append(System.lineSeparator());
+        expect.append("<p>Name; Hired; Fired; Salary;</p>").append(System.lineSeparator());
+        expect.append("<p>Name; Hired; Fired; Salary;</p>").append(System.lineSeparator());
+        expect.append("<p>").append(worker.getName()).append(";").append("<p>")
+                .append(System.lineSeparator())
+                .append("<p>").append(worker.getHired()).append(";").append("<p>")
+                .append(System.lineSeparator())
+                .append("<p>").append(worker.getFired()).append(";").append("<p>")
+                .append(System.lineSeparator())
+                .append("<p>").append(worker.getSalary()).append(";").append("<p>")
+                .append(System.lineSeparator());
+        expect.append("</body>").append(System.lineSeparator());
+        expect.append("/html").append(System.lineSeparator());
+        assertThat(engine.generateForProgrammers(em -> em.getName().equals("Ivan")), is(expect.toString()));
+    }
+
+    @Test
+    public void whenGenerateForBookkeeping() {
+        double tax = 0.13;
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        store.add(worker);
+
+        ReportEngine engine = new ReportEngine(store);
+
+        Employee expectedWorker = new Employee("Ivan", now, now, 100 * (1 - tax));
+        StringBuilder expect = new StringBuilder()
+                .append("Name; Hired; Fired; Salary;")
+                .append(System.lineSeparator())
+                .append(expectedWorker.getName()).append(";")
+                .append(expectedWorker.getHired()).append(";")
+                .append(expectedWorker.getFired()).append(";")
+                .append(expectedWorker.getSalary()).append(";")
+                .append(System.lineSeparator());
+        assertThat(engine.generateForBookkeeping(em -> em.getName().equals("Ivan")), is(expect.toString()));
+    }
+
+    @Test
+    public void whenGenerateForHR() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker1 = new Employee("Ivan", now, now, 100);
+        Employee worker2 = new Employee("Petr", now, now, 150);
+        store.add(worker1);
+        store.add(worker2);
+
+        ReportEngine engine = new ReportEngine(store);
+        StringBuilder expect = new StringBuilder()
+                .append("Name; Hired; Fired; Salary;")
+                .append(System.lineSeparator())
+                .append(worker2.getName()).append("; ")
+                .append(worker2.getSalary()).append(";")
+                .append(System.lineSeparator())
+                .append(worker1.getName()).append("; ")
+                .append(worker1.getSalary()).append(";")
+                .append(System.lineSeparator());
+
+        assertThat(engine.generateForHR(em -> !em.getName().isEmpty()), is(expect.toString()));
     }
 }
