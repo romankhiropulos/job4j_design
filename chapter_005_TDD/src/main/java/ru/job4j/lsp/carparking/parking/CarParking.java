945ee0195546c34dd1d32ab983485efff0a7db8a
romankhiropulos <romankhiropulos@gmail.com>
2020-11-18 01:22:10 +0300

    Car parking fixed

--- chapter_005_TDD/src/main/java/ru/job4j/lsp/carparking/parking/CarParking.java
+++ chapter_005_TDD/src/main/java/ru/job4j/lsp/carparking/parking/CarParking.java
@@ -2,14 +2,28 @@ package ru.job4j.lsp.carparking.parking;
 
 import ru.job4j.lsp.carparking.car.Car;
 
-public class CarParking extends AbstractParking {
+public class CarParking implements Parking {
+
+    private int carLimit;
+
+    private Car[] carStorage = new Car[carLimit];
 
     public CarParking(int carLimit) {
-        super(carLimit);
+        this.carLimit = carLimit;
     }
 
     @Override
     public boolean park(Car car) {
         return false;
+    }
+
+    @Override
+    public int getParkingSize() {
+        return carLimit;
+    }
+
+    @Override
+    public Car[] getParkingStorages() {
+        return carStorage;
     }
 }
